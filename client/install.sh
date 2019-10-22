f3ece4d8afd701df27d29354d53b37ffbb9e48c3
lolnsw <laurent@colobane.com>
2019-10-22 03:34:15 +0100

    install script

--- client/install.sh
+++ client/install.sh
@@ -1,14 +1,24 @@
+echo "Installing Python depedencies"
 mount -o remount rw, /
 cd /tmp
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 pip install pyserial
 
+echo ""
+echo "Enabling UART"
 mount -o remount, rw /boot
 echo "enable_uart=1" >> /boot/config.txt
 
+echo ""
+echo "Installing arcade-controller scripts"
 mkdir /recalbox/share/custom_scripts
-curl https://github.com/lolnsw/arcade_controller/tree/master/client/S25arcade-controller-client > /etc/init.d/S25arcade-controller-client
-curl https://github.com/lolnsw/arcade_controller/tree/master/client/arcade-controller-client.py > /recalbox/share/custom_scripts/arcade-controller-client.py
+curl https://raw.githubusercontent.com/lolnsw/arcade_controller/master/client/S25arcade-controller-client > /etc/init.d/S25arcade-controller-client
+curl hhttps://raw.githubusercontent.com/lolnsw/arcade_controller/master/client/arcade-controller-client.py> /recalbox/share/custom_scripts/arcade-controller-client.py
 chmod +x /etc/init.d/S25arcade-controller-client
 chmod +x /recalbox/share/custom_scripts/arcade-controller-client.py
+
+echo ""
+echo "Rebooting..."
+
+reboot
